eebd488c3cc1ffbc0c40af83b6c56ca94e88c428
ghelbert <gustavo_reinoso@usmp.pe>
2026-09-19 23:36:03 -0500

    w

--- src/main/java/com/example/mi_api/MiApiApplication.java
+++ src/main/java/com/example/mi_api/MiApiApplication.java
@@ -2,7 +2,7 @@ package com.example.mi_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//s
 @SpringBootApplication
 public class MiApiApplication {
 
